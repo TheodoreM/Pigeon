626f8bfdfe50bc726699ccafdf55b3fc1298eee1
TheodoreM <teo.mixalis@hotmail.com>
2022-11-23 18:59:05 +0200

    first designForLogin

---
+++ pigeon/src/pages/login/Login.jsx
@@ -0,0 +1,30 @@
+import "./login.scss"
+
+const Login = () => {
+    return (
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>Welcome Humans!</h1>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+                        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+                        qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem
+                    </p>
+                    <span>Don't you have an account?</span>
+                    <button>Register</button>
+                </div>
+                <div className="right">
+                    <h1>Login</h1>
+                    <form>
+                        <input type="text" placeholder="Username" />
+                        <input type="password" placeholder="Password" />
+                        <button>Login</button>
+                    </form>
+                </div>
+            </div>
+            Login
+        </div>
+    )
+}
+
+export default Login
